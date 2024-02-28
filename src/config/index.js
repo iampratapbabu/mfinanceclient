fe72036b158283d4e6a7e06fdbdecdac937539f5
Tej Pratap <bankebihari5463@gmail.com>
2024-02-28 23:27:24 +0530

    prod

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 //Backend URL:
 
 //local
-export const BASE_URL = "http://127.0.0.1:8000";
+//export const BASE_URL = "http://127.0.0.1:8000";
 
 //Stagging
-//export const BASE_URL = "http://142.44.247.133:8000";
+export const BASE_URL = "http://142.44.247.133:8000";
 
 //Live
 // export const BASE_URL = "http://tejtech.com";
