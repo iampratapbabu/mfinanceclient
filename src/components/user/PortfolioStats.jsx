be8dfb0b784724958bf95204862b71297f8f985c
Tej Pratap <bankebihari5463@gmail.com>
2024-09-24 00:08:32 +0530

    change

--- src/components/user/PortfolioStats.jsx
+++ src/components/user/PortfolioStats.jsx
@@ -1,53 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import ContentLoader from '../loader/ContentLoader';
-import axios from "axios";
-import { BASE_URL } from "../../config";
-import toast from 'react-hot-toast';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import {currencyFmt}  from '../../helper/stringMethods';
+import { currencyFmt } from '../../helper/stringMethods';
 
 
 
 
+//This is stateless component no useState
 const PortfolioStats = (props) => {
-
-    const [loading, setLoading] = useState(true);
-    const [portfolioData, setPortfolioData] = useState();
-    const [divSection, setDivSection] = useState("default");
-
-
 
     useEffect(() => {
         console.log("props passed", props);
-        loadPortfolio();
     }, [])
-
-
-    const loadPortfolio = async () => {
-        try {
-            setLoading(true);
-            const axiosRes = await axios({
-                method: "GET",
-                headers: { 'x-access-token': localStorage.getItem('token') },
-                url: `${BASE_URL}/api/portfolio`,
-            });
-            console.log("loadportfolio [SUCCESS]", axiosRes.data);
-            if (axiosRes.data.success) {
-                setLoading(false);
-                setPortfolioData(axiosRes.data.resData)
-                setDivSection("");
-            } else {
-                setLoading(false);
-
-            }
-        } catch (err) {
-            console.log("loadportfolio [ERROR]", err);
-            setLoading(false);
-            setDivSection("error");
-        }
-    }
 
     const reverseProp = () => {
         props.funProp("any value");
@@ -61,42 +23,35 @@ const PortfolioStats = (props) => {
         }
     }
 
-    if (loading) return (<ContentLoader />);
     return (
         <>
-            {
-                divSection === "error" ?
-                    <>
-                        <p>Portfolio Not Loaded </p>
-                        <Button onClick={loadPortfolio}>Try Again</Button>
-                    </>
-                    :
-                    <>
-                        <div className='summary-card'>
 
-                            <div className='portfolio-stats'>
-                                <div className=''>
-                                    <h6> Net Worth</h6><span className={getClassName(portfolioData?.netWorth)}>₹ {currencyFmt(portfolioData?.netWorth)}</span>
-                                </div>
-                                <div className=''>
-                                    <h6>Assets</h6><span>₹ {currencyFmt(portfolioData?.totalAssets)}</span>
-                                </div>
-                                <div className=''>
-                                    <h6> Liablities</h6><span>₹ {currencyFmt(portfolioData?.totalLiablites)}</span>
-                                </div>
-                            </div>
+            <div className='summary-card'>
 
-                            {/* 
-                            <Button variant="primary" onClick={reverseProp}>
-                                Reverse Prop
-                            </Button> */}
+                <div className='portfolio-stats'>
+                    <div className=''>
+                        <h6> Net Worth</h6><span className={getClassName(props?.userPortfolio?.netWorth)}>₹ {currencyFmt(props?.userPortfolio?.netWorth)}</span>
+                    </div>
+                    <div className=''>
+                        <h6>Assets</h6><span>₹ {currencyFmt(props?.userPortfolio?.totalAssets)}</span>
+                    </div>
+                    <div className=''>
+                        <h6> Liablities</h6><span>₹ {currencyFmt(props?.userPortfolio?.totalLiablites)}</span>
+                    </div>
+                </div>
+
+                {
+                    /* 
+                    <Button variant="primary" onClick={reverseProp}>
+                        Reverse Prop
+                    </Button> 
+                    */
+                }
 
 
-                        </div>
+            </div>
 
 
-                    </>
-            }
 
 
         </>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,6 +7,9 @@ import Modal from 'react-bootstrap/Modal';
 import { addPortfolio } from '../helper/httpHelper';
 import Form from 'react-bootstrap/Form';
 import toast from 'react-hot-toast';
+import axios from "axios";
+import { BASE_URL } from '../config';
+import ContentLoader from '../components/loader/ContentLoader';
 
 const HomePage = () => {
   const { authState } = useContext(Econtext);
@@ -15,14 +18,21 @@ const HomePage = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [portfolioData, setPortfolioData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [divSection, setDivSection] = useState("default");
+
+
   const [expense, setExpense] = useState({
     expenseType: "",
     amount: 0
   });
 
-  useEffect(()=>{
 
-  },[show])
+
+  useEffect(() => {
+    loadPortfolio();
+  }, [show])
 
 
   const funToRun = (param) => {
@@ -35,16 +45,16 @@ const HomePage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    createPortfolio();
+    addExpense();
   }
 
-  const createPortfolio = async () => {
+  const addExpense = async () => {
     try {
       const reqData = {};
 
       reqData.expenses = expense;
       console.log(reqData);
-      const serverRes = await addPortfolio(reqData,"expenses");
+      const serverRes = await addPortfolio(reqData, "expenses");
       toast.success(serverRes.message);
       setShow(false);
       console.log(serverRes);
@@ -53,7 +63,30 @@ const HomePage = () => {
     }
   }
 
-  
+  const loadPortfolio = async () => {
+    try {
+      setLoading(true);
+      const axiosRes = await axios({
+        method: "GET",
+        headers: { 'x-access-token': localStorage.getItem('token') },
+        url: `${BASE_URL}/api/portfolio`,
+      });
+      console.log("loadportfolio [SUCCESS]", axiosRes.data);
+      if (axiosRes.data.success) {
+        setLoading(false);
+        setPortfolioData(axiosRes.data.resData)
+        setDivSection("");
+      } else {
+        setLoading(false);
+
+      }
+    } catch (err) {
+      console.log("loadportfolio [ERROR]", err);
+      setLoading(false);
+      setDivSection("error");
+    }
+  }
+
 
   return (
     <>
@@ -65,128 +98,134 @@ const HomePage = () => {
           </button>
 
         </div>
+        {
+          loading ? <ContentLoader /> :
+            <>
+              <PortfolioStats custom={"this is custom prop"} funProp={funToRun} userPortfolio={portfolioData} />
 
-        <PortfolioStats custom={"this is custom prop"} funProp={funToRun} />
+              <div className='summary-card'>
 
-        <div className='summary-card'>
-
-          <h3 className='h3-title'>Assets</h3>
-          <div className='box-end'>
-            <div className=''>
-              <p>Saving Account</p>
-              <h6>as on 13 April 2024</h6>
-            </div>
-            <div className=''>
-              <h6> Rs 8.63</h6>
-              <button className='btn'>Detail</button>
-            </div>
-          </div>
-          <br />
-          <div className='box-end'>
-            <div className=''>
-              <p>Mutual Funds</p>
-              <h6>as on 13 April 2024</h6>
-            </div>
-            <div className=''>
-              <h6> Rs 1,75,000</h6>
-              <button className='btn'>Detail</button>
-            </div>
-          </div>
-
-
-        </div>
-
-
-        <div className='summary-card'>
-          <h3 className='h3-title'>Liablities</h3>
-          <div className='box-end'>
-            <div className=''>
-              <p>Personal Loan</p>
-              <h6>as on 13 April 2024</h6>
-            </div>
-            <div className=''>
-              <h6> Rs 54,677.84</h6>
-              <button className='btn'>Detail</button>
-            </div>
-          </div>
-          <br />
-          <div className='box-end'>
-            <div className=''>
-              <p>Credit Card</p>
-              <h6>as on 13 April 2024</h6>
-            </div>
-            <div className=''>
-              <h6> Rs 37,650.67</h6>
-              <button className='btn'>Detail</button>
-            </div>
-          </div>
+                <h3 className='h3-title'>Assets</h3>
+                <div className='box-end'>
+                  <div className=''>
+                    <p>Saving Account</p>
+                    <h6>as on 13 April 2024</h6>
+                  </div>
+                  <div className=''>
+                    <h6> Rs 8.63</h6>
+                    <button className='btn'>Detail</button>
+                  </div>
+                </div>
+                <br />
+                <div className='box-end'>
+                  <div className=''>
+                    <p>Mutual Funds</p>
+                    <h6>as on 13 April 2024</h6>
+                  </div>
+                  <div className=''>
+                    <h6> Rs 1,75,000</h6>
+                    <button className='btn'>Detail</button>
+                  </div>
+                </div>
 
 
-        </div>
-
-        <div className='summary-card'>
-          <h3 className='h3-title'>Expense</h3>
-          <br />
-          <h6>Filter On</h6><span>Last Week</span>
-          <hr />
-          <div className='box-end'>
-
-            <div className='box-end-col'>
-              <h6>Category</h6><span>Fuel</span>
-            </div>
-            <div className='box-end-col'>
-              <h6> Spent</h6><span>₹ {"23"}</span>
-            </div>
-          </div>
-          <hr />
-
-          <div className='box-end'>
-
-            <div className=''>
-              <h6>Category</h6><span>Fuel</span>
-            </div>
-            <div className=''>
-              <h6> Spent</h6><span>₹ {"23"}</span>
-            </div>
-          </div>
-
-          <hr />
-
-          <div className='box-end'>
-
-            <div className=''>
-              <h6>Category</h6><span>Fuel</span>
-            </div>
-            <div className=''>
-              <h6> Spent</h6><span>₹ {"23"}</span>
-            </div>
-          </div>
-
-          <hr />
-
-          <div className='box-end'>
-
-            <div className=''>
-              <h6>Category</h6><span>Fuel</span>
-            </div>
-            <div className=''>
-              <h6> Spent</h6><span>₹ {"23"}</span>
-            </div>
-          </div>
-          <hr />
-
-          <div className='box-end'>
-    
-            <div className=''>
-              <h6>Category</h6><span>Fuel</span>
-            </div>
-            <div className=''>
-              <h6> Spent</h6><span>₹ {"23"}</span>
-            </div>
-          </div>
+              </div>
 
 
-        </div>
+              <div className='summary-card'>
+                <h3 className='h3-title'>Liablities</h3>
+                <div className='box-end'>
+                  <div className=''>
+                    <p>Personal Loan</p>
+                    <h6>as on 13 April 2024</h6>
+                  </div>
+                  <div className=''>
+                    <h6> Rs 54,677.84</h6>
+                    <button className='btn'>Detail</button>
+                  </div>
+                </div>
+                <br />
+                <div className='box-end'>
+                  <div className=''>
+                    <p>Credit Card</p>
+                    <h6>as on 13 April 2024</h6>
+                  </div>
+                  <div className=''>
+                    <h6> Rs 37,650.67</h6>
+                    <button className='btn'>Detail</button>
+                  </div>
+                </div>
+
+
+              </div>
+
+              <div className='summary-card'>
+                <h3 className='h3-title'>Expense</h3>
+                <br />
+                <h6>Filter On</h6><span>Last Week</span>
+                <hr />
+                <div className='box-end'>
+
+                  <div className='box-end-col'>
+                    <h6>Category</h6><span>Fuel</span>
+                  </div>
+                  <div className='box-end-col'>
+                    <h6> Spent</h6><span>₹ {"23"}</span>
+                  </div>
+                </div>
+                <hr />
+
+                <div className='box-end'>
+
+                  <div className=''>
+                    <h6>Category</h6><span>Fuel</span>
+                  </div>
+                  <div className=''>
+                    <h6> Spent</h6><span>₹ {"23"}</span>
+                  </div>
+                </div>
+
+                <hr />
+
+                <div className='box-end'>
+
+                  <div className=''>
+                    <h6>Category</h6><span>Fuel</span>
+                  </div>
+                  <div className=''>
+                    <h6> Spent</h6><span>₹ {"23"}</span>
+                  </div>
+                </div>
+
+                <hr />
+
+                <div className='box-end'>
+
+                  <div className=''>
+                    <h6>Category</h6><span>Fuel</span>
+                  </div>
+                  <div className=''>
+                    <h6> Spent</h6><span>₹ {"23"}</span>
+                  </div>
+                </div>
+                <hr />
+
+                <div className='box-end'>
+
+                  <div className=''>
+                    <h6>Category</h6><span>Fuel</span>
+                  </div>
+                  <div className=''>
+                    <h6> Spent</h6><span>₹ {"23"}</span>
+                  </div>
+                </div>
+
+
+              </div>
+            </>
+        }
+
+
 
 
       </div>
@@ -196,14 +235,16 @@ const HomePage = () => {
           <Modal.Title>Record Expense</Modal.Title>
         </Modal.Header>
         <Form onSubmit={handleSubmit}>
-          <Modal.Body>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control name="expenseType" type="text" placeholder="Enter Expense Type" onChange={handleChange} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control name="amount" type="number" placeholder="Enter Amount" onChange={handleChange} />
-            </Form.Group>
-          </Modal.Body>
+          <div className='modal-form'>
+            <Modal.Body>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control name="expenseType" type="text" placeholder="Enter Expense Type" onChange={handleChange} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control name="amount" type="number" placeholder="Enter Amount" onChange={handleChange} />
+              </Form.Group>
+            </Modal.Body>
+          </div>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
