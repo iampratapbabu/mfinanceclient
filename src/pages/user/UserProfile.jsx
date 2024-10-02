196213140837acacbc6a2f0f873988cefaf092cb
Tej Pratap <bankebihari5463@gmail.com>
2024-10-02 22:00:49 +0530

    load user complete'

--- src/pages/user/UserProfile.jsx
+++ src/pages/user/UserProfile.jsx
@@ -3,6 +3,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Econtext } from '../../context/Econtext';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import ContentLoader from '../../components/loader/ContentLoader';
+import axios from "axios";
+import toast from 'react-hot-toast';
+import { BASE_URL } from '../../config';
+
 
 
 
@@ -10,12 +15,13 @@ import Button from 'react-bootstrap/Button';
 
 const UserProfile = () => {
 
-  const { authDispatch, logout, authState } = useContext(Econtext);
+  const { authDispatch, logout, authState,loadUser } = useContext(Econtext);
   const { user } = authState;
 
   const [currDiv, setCurrDiv] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const [signupInfo, setSignupInfo] = useState({
+  const [userInfo, setUserInfo] = useState({
     firstName: "",
     lastName: "",
     gender: "",
@@ -35,20 +41,43 @@ const UserProfile = () => {
 
 
   useEffect(() => {
+    setUserInfo(user);
   }, [])
 
 
   const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value })
+
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      const axiosRes = await axios({
+        method: "PATCH",
+        headers: { 'x-access-token': localStorage.getItem('token') },
+        url: `${BASE_URL}/api/users/single`,
+        data:userInfo
+      });
+      console.log("editProfile [SUCCESS]", axiosRes.data);
+      if (axiosRes.data.success) {
+        loadUser();
+        setCurrDiv("");
+        setLoading(false);
+      } else {
+        setLoading(false);
 
+      }
+    } catch (err) {
+      console.log("editProfile [ERROR]", err);
+      setLoading(false);
+    }
 
   }
 
 
-
+  if (loading) return <ContentLoader />
   return (
     <>
 
@@ -108,25 +137,25 @@ const UserProfile = () => {
               <Form onSubmit={handleSubmit}>
 
                 <Form.Group className="mb-3" controlId="formFirstName">
-                  <Form.Control className='shadow-none' name="firstName" type="text" placeholder="First Name" onChange={handleChange} />
+                  <Form.Control className='shadow-none' name="firstName" type="text" placeholder="First Name" onChange={handleChange} value={userInfo?.firstName || ""} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formLastName">
-                  <Form.Control className='shadow-none' name="lastName" type="text" placeholder="Last Name" onChange={handleChange} />
+                  <Form.Control className='shadow-none' name="lastName" type="text" placeholder="Last Name" onChange={handleChange} value={userInfo?.lastName || ""} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" >
-                  <Form.Select aria-label="Select Gender" name="gender" onChange={handleChange}>
+                  <Form.Select aria-label="Select Gender" name="gender" onChange={handleChange} value={userInfo?.gender || ""}>
                     <option>Select Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                   </Form.Select>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Control className='shadow-none' name="email" type="email" placeholder="Email" onChange={handleChange} />
+                  <Form.Control className='shadow-none' name="email" type="email" placeholder="Email" onChange={handleChange} value={userInfo?.email || ""} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicNumber">
-                  <Form.Control className='shadow-none' name="phone" type="text" placeholder="Phone Number" onChange={handleChange} />
+                  <Form.Control className='shadow-none' name="phone" type="text" placeholder="Phone Number" onChange={handleChange} value={userInfo?.phone || ""} />
                   <Form.Text className="text-muted">
                   </Form.Text>
                 </Form.Group>
